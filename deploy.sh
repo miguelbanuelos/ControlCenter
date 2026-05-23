2f4bf7b285d7c4bf0a9f005cb6e5a8a8a22d5670
Migue Bañuelos <kaiser.gdl@gmail.com>
2026-05-23 15:09:09 -0600

    more things

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ cd /docker/ControlCenter
 git config --global --add safe.directory /docker/ControlCenter
 git fetch --all
 git reset --hard origin/main
-docker compose up -d --build --force-recreate
+#docker compose up -d --build --force-recreate
+nohup sh -c 'sleep 3 && docker compose up -d --build --force-recreate' > /dev/null 2>&1 &
 #(sleep 3 && docker compose up -d --build) &
